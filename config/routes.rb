8934ecee1589fbf602ef8671443d5fc07d0bd182
GregPK <grzegorz.kaczorek@gmail.com>
2015-06-25 22:42:58 +0200

    Fix routes to point to achievers instead of exposing a very general API

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'Achiever', at: 'api/v1/'
+  mount_devise_token_auth_for 'Achiever', at: 'api/v1/achievers'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
